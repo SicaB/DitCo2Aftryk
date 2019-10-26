f5717df28f10d57677e7f153a9dc1f5c899c37f7
Sacha Behrend <sacha.behrend@gmail.com>
2019-10-26 21:26:10 +0200

    Added TextWatcher to make calculation on flightCo2 display in real time.

--- app/src/main/java/com/example/ditco2aftryk/view/ui/FlightCo2Activity.kt
+++ app/src/main/java/com/example/ditco2aftryk/view/ui/FlightCo2Activity.kt
@@ -3,6 +3,9 @@ package com.example.ditco2aftryk.view.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.ditco2aftryk.R
@@ -12,23 +15,40 @@ import com.example.ditco2aftryk.utils.toast
 import com.example.ditco2aftryk.view.viewmodel.FlightCo2ViewModel
 import kotlinx.android.synthetic.main.activity_flight_co2.*
 
-class FlightCo2Activity : AppCompatActivity(), Listener {
+class FlightCo2Activity : AppCompatActivity(), Listener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Bind this activity to the layout xml file using databinding
-        val binding : ActivityFlightCo2Binding = DataBindingUtil.setContentView(this, R.layout.activity_flight_co2)
-
         // create the viewModel
         val viewModel = ViewModelProviders.of(this).get(
             FlightCo2ViewModel::class.java)
+
+        // Bind this activity to the layout xml file using databinding
+        val binding : ActivityFlightCo2Binding = DataBindingUtil.setContentView(this, R.layout.activity_flight_co2)
 
         // bind this activity to the viewModel
         binding.viewmodel = viewModel
 
         // define listener to the viewModel
         viewModel.listener = this
+
+        enterHoursFlown.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!enterHoursFlown.text.isEmpty()) {
+                    val hoursFlown = enterHoursFlown.text.toString().toDouble()
+                    val calculatedFlightCo2 = hoursFlown * 0.092
+                    calculatedCo2TextField.text = String.format("%.2f", calculatedFlightCo2)
+                    Log.d("TAG", "hej hej")
+                }
+            }
+        })
     }
 
     override fun onSuccess() {
--- app/src/main/java/com/example/ditco2aftryk/view/viewmodel/FlightCo2ViewModel.kt
+++ app/src/main/java/com/example/ditco2aftryk/view/viewmodel/FlightCo2ViewModel.kt
@@ -11,15 +11,14 @@ import com.example.ditco2aftryk.model.repositories.Co2CountRepository
 import com.example.ditco2aftryk.view.ui.Listener
 import kotlinx.coroutines.launch
 
-class FlightCo2ViewModel(application: Application) : AndroidViewModel(application){
+class FlightCo2ViewModel(application: Application) : AndroidViewModel(application) {
 
     // ViewModel maintains a reference to the repository to get data.
     private val repository: Co2CountRepository
-    val accumulatedCo2Count: LiveData<String>
-
-    var flightCo2Input: String? = null
 
     var listener: Listener? = null
+
+    val flightCo2Input = MutableLiveData<String>()
 
     private lateinit var input: Co2Count
 
@@ -28,22 +27,30 @@ class FlightCo2ViewModel(application: Application) : AndroidViewModel(applicatio
         // the correct Co2CountRepository.
         val co2CountDao = AppDatabase.invoke(application).getCo2CountDao()
         repository = Co2CountRepository(co2CountDao)
-        accumulatedCo2Count = repository.accumulatedCo2Counts
     }
 
-    // Function to save users input in the database
+    // Calculation of flight co2 based on input
+    fun calculateFlightCo2(input: String) : String{
+        val flightCo2InTon = input.toInt() * 0.092
+        return flightCo2InTon.toString()
+    }
+
+    // Function to save user input in the database when button is clicked
     fun onSaveCo2ButtonClick(@Suppress("UNUSED_PARAMETER")view: View){
-        if(!flightCo2Input?.isDigitsOnly()!! || flightCo2Input.isNullOrEmpty()){
+        if(!flightCo2Input.value?.isDigitsOnly()!! || flightCo2Input.value.isNullOrEmpty()){
             listener?.onFailure("Invalid co2 value entered.")
             return
         }
-        input = Co2Count(0, flightCo2Input!!)
+
+        input = Co2Count(0, calculateFlightCo2(flightCo2Input.value!!))
         insert(input)
         listener?.onSuccess()
     }
 
+    // Function to insert user input using a coroutine
     fun insert(co2Count: Co2Count) = viewModelScope.launch {
         Log.d("MyTag", "Inserted")
         repository.saveCo2Count(co2Count)
     }
+
 }
